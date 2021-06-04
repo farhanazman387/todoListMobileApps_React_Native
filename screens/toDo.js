f1b113dcc91ab501d3fc03424ba580f7b80d0661
Farhana Azman <57706565+farhanazman387@users.noreply.github.com>
2021-06-04 15:25:49 +0800

    Add files via upload
    added some new component and checkbox extension to component

--- screens/toDo.js
+++ screens/toDo.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import React, { useState } from 'react'
-import {View, Text, TextInput, KeyboardAvoidingView, TouchableOpacity} from 'react-native'
+import {View, Text, TextInput, KeyboardAvoidingView, TouchableOpacity, Keyboard} from 'react-native';
+import RemoveTaskbtn from '../components/Removebtn';
 import TodoItem from '../components/TodoItem';
 import styles from '../styles/styles';
 
@@ -11,23 +12,46 @@ const Todo=() =>{
     const [taskAddItem, setTaskItem] = useState([]);
 
     const handleAddTask = () => {
-        setTaskItem([...taskAddItem,taskAdd]),setTask(null);
+        Keyboard.dismiss();
+        setTaskItem([...taskAddItem,taskAdd])
+        setTask(null);
     };
+
+    const completedTask = (index) => {
+        let taskCopy = [...taskAddItem];
+        taskCopy.splice(index,1);
+        setTaskItem(taskCopy);
+    };
+
     return(
     <View>
         <View>
             <Text style={styles.main_text}>{currentDate}</Text>
             <View style={styles.line}></View>
-            {taskAddItem.map((item) => {
-                return <TodoItem Text={item}/>;
-            })}
+
+            <View>
+                <Text style={styles.main_text_title}>Today's Task</Text>
+            </View>
+            <View style={styles.item_task}>
+                {taskAddItem.map((item, index) => {
+                    return (
+                        // <TouchableOpacity key={index}  onPress={() => completedTask(index)}>
+                        //     <TodoItem Text={item}/>
+                        // </TouchableOpacity>
+                        <TodoItem Text={item} TouchableOpacity={index}/>
+                    );
+                    
+                })}
+            </View>
         </View>
         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-            <View style={styles.todo_wrapper}>
+            <View style={styles.addTask_wrapper}>
                 <TextInput style={styles.task_input} placeholder="Add your task here..."
                     onChangeText={(text) => setTask(text)}/>
                 <TouchableOpacity onPress={()=> handleAddTask()}>
-                    <Text style={styles.task_add_button}>+</Text>
+                    <View style={styles.task_add_button}>
+                        <Text style={styles.task_add_btn_text}>+</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
